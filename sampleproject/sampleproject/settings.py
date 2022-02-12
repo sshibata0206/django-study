8959645f4a717bacccccae6eab6a17d7a9d26c01
sshibata <bacchi0206@gmail.com>
2022-02-12 17:34:29 +0900

    Change the place of seacret key

--- sampleproject/sampleproject/settings.py
+++ sampleproject/sampleproject/settings.py
@@ -11,16 +11,14 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from .local_settings import SECRET_KEY
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c(99276gnq!dgmolst1hsr8&(!#8p)_-*ixv$2jpjk1map9&b7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
