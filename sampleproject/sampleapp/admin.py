d954a1e1087c619977ecf8ca4c94b63617eeb6af
sshibata <bacchi0206@gmail.com>
2022-02-12 17:03:08 +0900

    first commit

---
+++ sampleproject/sampleapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import SampleAppModel
+
+# Resister your models here.
+
+admin.site.register(SampleAppModel)
---
+++ sampleproject/sampleapp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+class SampleAppModel(models.Model):
+    itemname = models.CharField(max_length=100)
+    itemvalue = models.IntegerField()
